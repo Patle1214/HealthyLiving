6046852b19959675625107f594c78459faba2607
Patle1214 <63815626+Patle1214@users.noreply.github.com>
2023-06-08 17:24:41 -0700

    Add files via upload

---
+++ HealthyLivingProject/backend/data.py
@@ -0,0 +1,11 @@
+import json
+#def test(location,goal):
+def test():
+    with open('result.json') as json_file:
+        data = json.load(json_file)
+        #if goal == 1: #bulk
+            #sort(data[location],calories/protein)
+        #return(data[location])
+        return data
+    
+print(test("IRVINE"))
